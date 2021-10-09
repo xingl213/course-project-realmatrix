e5f741f30f87b39bd6cd673bdd7a416fc6ee3611
ziqishu <ziqi.shu@mail.utoronto.ca>
2021-10-09 17:26:58 -0400

    Card and CardManager

---
+++ src/main/java/entity/Card.java
@@ -0,0 +1,86 @@
+package entity;
+
+/**
+ * Card class contains a term and its definition.
+ */
+
+public class Card {
+
+    private final String id;
+    private String term;
+    private String definition;
+    private int proficiency;
+
+    /**
+      Construct a Card, giving them the given id,
+      term, definition, and proficiency.
+
+      @param id ID of the Card
+     * @param term Term that Card contains
+     * @param definition Definition of the Term that Card contains
+     * @param proficiency  Proficiency of the Term that Card contains
+     */
+    public Card(String id, String term, String definition, int proficiency){
+        this.id = id;
+        this.term = term;
+        this.definition = definition;
+        this.proficiency = proficiency;
+    }
+
+    /**
+     * Getter for the ID of the Cards as a String
+     * @return id as a String
+     */
+    public String getId(){
+        return this.id;
+    }
+
+    /**
+     * Getter for the term of the Cards as a String
+     * @return term as a String
+     */
+    public String getTerm(){
+        return this.term;
+    }
+
+    /**
+     * Getter for the definition of the Cards as a String
+     * @return definition as a String
+     */
+    public String getDefinition(){
+        return this.definition;
+    }
+
+    /**
+     * Change the term to target term
+     * @param target_term the target term we want to change to
+     */
+    public void changeTerm(String target_term){
+        this.term = target_term;
+    }
+
+    /**
+     * Change the definition to target definition
+     * @param target_definition the target definition we want to change to
+     */
+    public void changeDefinition(String target_definition){
+        this.definition = target_definition;
+    }
+
+    /**
+     * Increase the proficiency with increase value
+     * @param increase_value the increase value we want to add to the proficiency
+     */
+    public void increaseProficiency(int increase_value){
+        this.proficiency += increase_value;
+    }
+
+
+    /**
+     * Represents the current Card by its term and definition
+     * @return the name and student number separated by a comma
+     */
+    public String toString() {
+        return this.getTerm() + ": " + this.getDefinition();
+    }
+}
---
+++ src/main/java/entity/CardManager.java
@@ -0,0 +1,55 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the entire system of Cards
+ */
+public class CardManager {
+    private List<Card> cards;
+
+    /**
+     * Creates a CardManager with lists of Card that are empty
+     */
+    public CardManager() {
+        cards = new ArrayList<>();
+    }
+
+    /**
+     * Adds an instance of Card to the overall list of Cards
+     * @param id ID of the Card
+     * @param term Term that Card contains
+     * @param definition Definition of the Term that Card contains
+     * @param proficiency  Proficiency of the Term that Card contains
+     */
+    public void addCard(String id, String term, String definition, int proficiency) {
+        Card c = new Card(id, term, definition, proficiency);
+        cards.add(c);
+    }
+    // We can generate id here?
+
+    /**
+     * Update proficiencies of all Card in the system.
+     * @param change_value The value we want to change to Proficiency.
+     */
+    public void updateAllProficiency(int change_value){
+        for (Card c : cards) {
+            c.increaseProficiency(change_value);
+        }
+    }
+
+    /**
+     * Represents a CardManager as a String containing all Card term and definition in the system.
+     * @return a list of card terms and definitions, separated by comma.
+     */
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for (Card c : cards) {
+            res.append(c.toString());
+            res.append(", ");
+        }
+        return res.toString(); //includes a trailing ", "
+    }
+}
